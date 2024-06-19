46290371ecf9eeb4f0c362339019d2c39fa861e7
unknown <milagros.day53@gmail.com>
2024-06-19 16:02:29 -0300

    modificamos cambios

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('nasa_image_gallery.urls'))
+    path('', include('nasa_image_gallery.urls')),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
